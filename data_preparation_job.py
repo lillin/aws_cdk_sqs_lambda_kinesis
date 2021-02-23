83c1c14dbd7d33e3a2be5c704a4c498ee747f237
lina <kleine.adi@gmail.com>
2021-02-23 13:08:44 +0200

    Uncomment services, add test transformations to job

--- data_preparation_job.py
+++ data_preparation_job.py
@@ -2,6 +2,10 @@ import sys
 import pyspark
 import boto3
 
+from awsglue.transforms import *
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.sql.types import *
+from pyspark.sql import Row
 from awsglue.context import GlueContext
 from awsglue.utils import getResolvedOptions
 from awsglue.job import Job
@@ -13,19 +17,51 @@ glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 
+order_list = [
+               ['1005', '623', 'YES', '1418901234', '75091'],\
+               ['1006', '547', 'NO', '1418901256', '75034'],\
+               ['1007', '823', 'YES', '1418901300', '75023'],\
+               ['1008', '912', 'NO', '1418901400', '82091'],\
+               ['1009', '321', 'YES', '1418902000', '90093']\
+             ]
+
+# Define schema for the order_list
+order_schema = StructType([  
+                      StructField("order_id", StringType()),
+                      StructField("customer_id", StringType()),
+                      StructField("essential_item", StringType()),
+                      StructField("timestamp", StringType()),
+                      StructField("zipcode", StringType())
+                    ])
+
+# Create a Spark Dataframe from the python list and the schema
+df_orders = spark.createDataFrame(order_list, schema = order_schema)
+df_orders.show()
+
+dyf_orders = DynamicFrame.fromDF(df_orders, glueContext, "dyf")
+dyf_applyMapping = ApplyMapping.apply( frame = dyf_orders, mappings = [ 
+  ("order_id","String","order_id","Long"), 
+  ("customer_id","String","customer_id","Long"),
+  ("essential_item","String","essential_item","String"),
+  ("timestamp","String","timestamp","Long"),
+  ("zipcode","String","zip","Long")
+])
+
+dyf_applyMapping.printSchema()
+
 # logic
-client = boto3.client('s3')
-s3_resource = boto3.resource('s3')
+# client = boto3.client('s3')
+# s3_resource = boto3.resource('s3')
 
-bucket_objects = client.list_objects_v2(Bucket='source-panini-333-bucket')
-keys = [obj['Key'] for obj in bucket_objects['Contents']]
+# bucket_objects = client.list_objects_v2(Bucket='source-panini-333-bucket')
+# keys = [obj['Key'] for obj in bucket_objects['Contents']]
 
-for key in keys:
-    copy_source = {
-        'Bucket': 'source-panini-333-bucket',
-        'Key': key
-    }
-    s3_resource.Object('storage-panini-333-bucket', key).copy(copy_source)
+# for key in keys:
+#     copy_source = {
+#         'Bucket': 'source-panini-333-bucket',
+#         'Key': key
+#     }
+#     s3_resource.Object('storage-panini-333-bucket', key).copy(copy_source)
 
 # end of job
 job.commit()
--- lib/cdk-sqs-l-qldb-stack.ts
+++ lib/cdk-sqs-l-qldb-stack.ts
@@ -1,9 +1,9 @@
 import * as cdk from '@aws-cdk/core';
-// import { Function, Runtime, Code, StartingPosition } from '@aws-cdk/aws-lambda';
-// import { SqsEventSource, KinesisEventSource } from '@aws-cdk/aws-lambda-event-sources';
-// import { Queue } from '@aws-cdk/aws-sqs';
-// import { CfnLedger, CfnStream } from '@aws-cdk/aws-qldb';
-// import { Stream } from '@aws-cdk/aws-kinesis';
+import { Function, Runtime, Code, StartingPosition } from '@aws-cdk/aws-lambda';
+import { SqsEventSource, KinesisEventSource } from '@aws-cdk/aws-lambda-event-sources';
+import { Queue } from '@aws-cdk/aws-sqs';
+import { CfnLedger, CfnStream } from '@aws-cdk/aws-qldb';
+import { Stream } from '@aws-cdk/aws-kinesis';
 
 import * as glue from '@aws-cdk/aws-glue';
 import * as iam from '@aws-cdk/aws-iam';
@@ -15,97 +15,97 @@ export class CdkSqsLQldbStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    // const queue = new Queue(this, 'queue', {
-    //   queueName: 'queue',
-    // });
+    const queue = new Queue(this, 'queue', {
+      queueName: 'queue',
+    });
 
-    // const myLambda = new Function(this, 'MyLambda', {
-    //   // The name of the method within your code that Lambda calls to execute your function. 
-    //   // The format includes the file name. 
-    //   handler: 'save-data-handler.handler',
-    //   code: Code.fromAsset('./handlers'),
-    //   runtime: Runtime.PYTHON_3_8,
-    // });
+    const myLambda = new Function(this, 'MyLambda', {
+      // The name of the method within your code that Lambda calls to execute your function. 
+      // The format includes the file name. 
+      handler: 'save-data-handler.handler',
+      code: Code.fromAsset('./handlers'),
+      runtime: Runtime.PYTHON_3_8,
+    });
 
-    // myLambda.addEventSource(new SqsEventSource(queue));
+    myLambda.addEventSource(new SqsEventSource(queue));
 
-    // const ledgerName = 'my-db'
-    // new CfnLedger(this, 'MyDB', {
-    //   permissionsMode: 'ALLOW_ALL',
-    //   name: ledgerName,
-    // });
+    const ledgerName = 'my-db'
+    new CfnLedger(this, 'MyDB', {
+      permissionsMode: 'ALLOW_ALL',
+      name: ledgerName,
+    });
 
     // add statement to lambda's policy to be able to interract with QLDB
-    // myLambda.addToRolePolicy(new iam.PolicyStatement({
-    //   effect: iam.Effect.ALLOW, 
-    //   actions: [ 'qldb:*' ], 
-    //   resources: [ `arn:aws:qldb:us-west-2:765423797119:ledger/${ledgerName}` ],
-    // }));
+    myLambda.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW, 
+      actions: [ 'qldb:*' ], 
+      resources: [ `arn:aws:qldb:us-west-2:765423797119:ledger/${ledgerName}` ],
+    }));
 
     // add Kinesis stream to write QLDB stream
-    // const kinesisStream = new Stream(this, 'MyKinesisStream', {
-    //     streamName: 'my-kinesis-stream'
-    // });
+    const kinesisStream = new Stream(this, 'MyKinesisStream', {
+        streamName: 'my-kinesis-stream'
+    });
 
     // create role for principal: QLDB service
-    // const qldbStreamRole = new iam.Role(this, 'QLDBStreamRole', {
-    //   assumedBy: new iam.ServicePrincipal('qldb.amazonaws.com')
-    // });
+    const qldbStreamRole = new iam.Role(this, 'QLDBStreamRole', {
+      assumedBy: new iam.ServicePrincipal('qldb.amazonaws.com')
+    });
 
-    // const qldbStreamPolicyStatement = new iam.PolicyStatement({
-    //   effect: iam.Effect.ALLOW,
-    //   actions: [ 'kinesis:PutRecord*', 'kinesis:DescribeStream', 'kinesis:ListShards' ],
-    //   resources: [ kinesisStream.streamArn ],
-    // })
+    const qldbStreamPolicyStatement = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [ 'kinesis:PutRecord*', 'kinesis:DescribeStream', 'kinesis:ListShards' ],
+      resources: [ kinesisStream.streamArn ],
+    })
 
-    // new iam.Policy(this, 'QLDBStreamPolicy', {
-    //   roles: [ qldbStreamRole ],
-    //   statements: [ qldbStreamPolicyStatement ], 
-    // });
+    new iam.Policy(this, 'QLDBStreamPolicy', {
+      roles: [ qldbStreamRole ],
+      statements: [ qldbStreamPolicyStatement ], 
+    });
 
     // create Amazon QLDB journal stream
-    // new CfnStream(this, 'MyQLDBStream', {
-    //   inclusiveStartTime: new Date('26 November 2020 00:00 UTC').toISOString(),
-    //   ledgerName: ledgerName,
-    //   roleArn: qldbStreamRole.roleArn,
-    //   streamName: `${ledgerName}-stream`,
-    //   kinesisConfiguration: {streamArn: kinesisStream.streamArn},
-    // });
+    new CfnStream(this, 'MyQLDBStream', {
+      inclusiveStartTime: new Date('26 November 2020 00:00 UTC').toISOString(),
+      ledgerName: ledgerName,
+      roleArn: qldbStreamRole.roleArn,
+      streamName: `${ledgerName}-stream`,
+      kinesisConfiguration: {streamArn: kinesisStream.streamArn},
+    });
 
     // If your function returns an error, Lambda retries the batch until processing succeeds or the data expires. 
     // To avoid stalled shards, you can configure the event source mapping to retry with a smaller batch size, 
     // limit the number of retries, or discard records that are too old.
 
-    // const queueBenefiaCompany = new Queue(this, 'queue-benefia-company', {
-    //   queueName: 'queue-benefia-company'
-    // });
+    const queueBenefiaCompany = new Queue(this, 'queue-benefia-company', {
+      queueName: 'queue-benefia-company'
+    });
 
-    // const queueErgohestiaCompnay = new Queue(this, 'queue-ergohestia-company', {
-    //   queueName: 'queue-ergohestia-company'
-    // });
+    const queueErgohestiaCompnay = new Queue(this, 'queue-ergohestia-company', {
+      queueName: 'queue-ergohestia-company'
+    });
 
     // create lambda to read from Kinesis stream
-    // const kinesisLambda = new Function(this, 'KinesisLambda', {
-    //   handler: 'kinesis-stream-handler.handler',
-    //   code: Code.fromAsset('./handlers'),
-    //   runtime: Runtime.PYTHON_3_8,
-    // });
+    const kinesisLambda = new Function(this, 'KinesisLambda', {
+      handler: 'kinesis-stream-handler.handler',
+      code: Code.fromAsset('./handlers'),
+      runtime: Runtime.PYTHON_3_8,
+    });
 
     // add to lambda's role policy statement to be able to manage Kinesis stream
-    // kinesisLambda.addToRolePolicy(new iam.PolicyStatement({
-    //   effect: iam.Effect.ALLOW,
-    //   actions: [        
-    //     'sqs:DeleteMessage',
-    //     'sqs:GetQueueAttributes',
-    //     'sqs:SendMessage',
-    //   ],
-    //   resources: [ queueBenefiaCompany.queueArn, queueErgohestiaCompnay.queueArn ]
-    // }));
+    kinesisLambda.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [        
+        'sqs:DeleteMessage',
+        'sqs:GetQueueAttributes',
+        'sqs:SendMessage',
+      ],
+      resources: [ queueBenefiaCompany.queueArn, queueErgohestiaCompnay.queueArn ]
+    }));
 
     // subscribe lambda to event
-    // kinesisLambda.addEventSource(new KinesisEventSource(kinesisStream, {
-    //   startingPosition: StartingPosition.TRIM_HORIZON  // return oldest records from shard first
-    // }));
+    kinesisLambda.addEventSource(new KinesisEventSource(kinesisStream, {
+      startingPosition: StartingPosition.TRIM_HORIZON  // return oldest records from shard first
+    }));
 
     new Bucket(this, 'MySourceBucket', {
       bucketName: 'source-panini-333-bucket'
@@ -197,12 +197,12 @@ export class CdkSqsLQldbStack extends cdk.Stack {
       maxRetries: 3,
     });
 
-    // const glueDevEndpoint = new glue.CfnDevEndpoint(this, 'MyDevEndpoint', {
-    //   roleArn: glueRole.roleArn,
-    //   endpointName: 'my-dev-endpoint',
-    //   glueVersion: '1.0',
-    //   arguments: {"GLUE_PYTHON_VERSION": "3.8"}
-    // });
+    new glue.CfnDevEndpoint(this, 'MyDevEndpoint', {
+      roleArn: glueRole.roleArn,
+      endpointName: 'my-dev-endpoint',
+      glueVersion: '1.0',
+      arguments: {"GLUE_PYTHON_VERSION": "3.8"}
+    });
 
   }
 }
